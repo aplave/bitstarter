9191b2cf53094cbaef18e1225b93f4281d922d84
Ubuntu <ubuntu@ip-172-31-47-159.us-west-2.compute.internal>
2013-07-15 04:31:52 +0000

    still trying

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
-var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFileSync('index.html',utf-8);
-  response.send('Hello World 2!');
-    
+  var fs = require('fs')
+  var buffer = new buffer ();
+  response.send(buffer.toString('utc 8',fs.readFileSync('index.html')))}); 
 });
 
 var port = process.env.PORT || 5000;
